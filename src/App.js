abee30947c473c7ece44ad7c788c839793b0448a
nicbuitr <n.buitrago29@uniandes.edu.co>
2017-03-09 09:26:22 -0500

    Not yet working but trying to

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './ImageSearch.js';
+var ImageSearch = require("./ImageSearch.js");
 
 class App extends Component {
   render() {
--- src/ImageSearch.js
+++ src/ImageSearch.js
@@ -19,22 +19,18 @@ module.exports = React.createClass({
         console.log('There has been a problem with your fetch operation: ' + error.message);
       });
     },
+    getUrl:function() {
+        return "https://farm"+
+            this.props.photo.farm +
+            ".staticflickr.com/" +
+            this.props.photo.server +
+            "/"+
+            this.props.photo.id+
+            "_" +
+            this.props.photo.secret +
+            "_s.jpg";
+    },
     render:function(){
-      var rows = [];
-      for (var i = 0; i < criterias.length; i++) {
-        rows.push(
-            <div className="form-group" key={"criteria_form_group_"+(i+1)}>
-              <div className="row" key={"criteria_row_"+(i+1)}>
-                <div className="col-md-11" key={"criteria_label_"+(i+1)}>
-                  <label className="control-label" key={"criteria_control_label_"+(i+1)} htmlFor={"criteria_"+(i+1)}>{criterias[i].description}</label>
-                </div>
-                <div className="col-md-1 text-right" key={"criteria_text_right_"+(i+1)}> 
-                  <input type="checkbox" className="criteria-checkbox" key={"criteria_checkbox"+(i+1)} id={"criteria_"+(i+1)} name={"criteria_"+(i+1)} value={criterias[i].selection} onChange={this.handleInputChange} />
-                </div>
-              </div>
-            </div>
-          );
-      }
         return(
           <div className="jumbotron">
             <div className="row text-center">
@@ -44,7 +40,7 @@ module.exports = React.createClass({
             <form className="form" onSubmit={this.searchImage}>             
               <div className="form-group">
                 <label className="control-label" htmlFor="query">Search images of: </label>
-                <input className="form-control" type="text" id="query" name="query" value={this.state.query} placeholder="Insert a search term"></textarea>                    
+                <input className="form-control" type="text" id="query" name="query" value={this.state.query} placeholder="Insert a search term"></input>                    
               </div>
               <div className="form-group text-center">
                 <button className="btn-lg" type="submit"  id="reviews-div">Search Image</button>
